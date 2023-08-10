1f46d9e8b61e5b6efb4a1e8fce500c539d5193cb
soefyan45 <soefyan45@gmail.com>
2023-08-10 09:48:18 +0700

    first comit

---
+++ components/Tutorial.vue
@@ -0,0 +1,31 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div class="flex justify-center pt-8 sm:pt-0" href="https://nuxtjs.org" target="_blank">
+        <img class="h-56" src="/icon.png">
+      </div>
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Kuis Sambung Ayat Al-Qur'an
+        </h2>
+        <p class="mt-3 text-gray-600">
+          Game ini merupakan sebuah permainan interaktif yang mana dapat membantu pemain dalam hal untuk belajar dan mengingat ayat-ayat al-qur’an, terkhusus pada ayat-ayat al-qur’an pada juz 30 atau juz amma. Format permainan pada game kuis sambung ayat al-qur’an ini adalah kuis dengan bentuk pilihan ganda. Yang mana pada permainan ini terdapat sebuah soal dan juga tersedia 4 buah opsi pilihan jawaban yang bisa ditebak oleh pemain.Pada permainan, pemain akan diberikan sebuah soal yang berupa potongan dari pada ayat Al-Qur’an dan pemain harus dapat menjawab dengan betul sambungan ayat dari pada soal yang telah diberikan. Aturan permainan ini adalah pemain akan di berikan kesempatan melakukan kesalahan sebanyak 4 kali kesalahan. Apabila pemain melakukan kesalahan lebih dari 4 kali, maka permainan akan berakhir.
+        </p>
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed flex">
+          <a href="/game.apk" class="py-2 px-4 rounded-md text-white font-semibold hove:bg-blue-600 mx-auto bg-blue-400 text-center justify-center">Download</a>
+        </p>
+      </div>
+      <div class="flex justify-center pt-4 space-x-2">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
